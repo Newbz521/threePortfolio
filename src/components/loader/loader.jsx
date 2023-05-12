080ab237edd82030ee5bcdb6380069d7761676f6
newbz521 <lawrence.yee1991@gmail.com>
2023-05-12 18:07:20 -0400

    2d loader

--- src/components/loader/loader.jsx
+++ src/components/loader/loader.jsx
@@ -1,10 +1,47 @@
-import { useProgress } from "@react-three/drei";
 
-export const LoadingScreen = ({ started, onStarted }) => {
+// import { useControls } from "leva";
+import { ReactNode, useRef, useState, useEffect } from "react";
+// import { Group } from "three";
+import {useProgress} from "@react-three/drei"
+
+
+function Loader(props) {
   const { progress } = useProgress();
+
+
+
+  
   return (
     <div className="loader-container">
-      {`${progress}%`}
+     <div className="charMovement">
+                                  <div className="largeContainer">
+                                    <div className="boxer">
+                                      <div className="character">
+                                        <div className="head">
+                                          <div className="face">
+                                            <div className="eyes"></div>
+                                            <div className="eyes2"></div>
+                                          </div>
+                                        </div>
+                                        <div className="upperBody">
+                                          <div className="leftArm"></div>
+                                          <div className="torso">
+                                            <div className="heart"></div>
+                                          </div>
+                                          <div className="rightArm"></div>
+                                        </div>
+                                        <div className="lowerBody">
+                                          <div className="leftLeg"></div>
+                                        </div>
+                                      </div>
+                                    </div>
+                                  </div>
+      </div>
+      <div className="loader-tag">{`${progress}%`}</div>
     </div>
   );
-};
+}
+
+
+export default Loader;
+
