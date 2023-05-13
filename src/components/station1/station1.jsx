d98499158be56464f35f7242030c9c21495ef8ef
newbz521 <lawrence.yee1991@gmail.com>
2023-05-13 10:28:46 -0400

    quick load edit

--- src/components/station1/station1.jsx
+++ src/components/station1/station1.jsx
@@ -463,7 +463,10 @@ function EyeAnimation({ preset }) {
       </mesh>
     )
   }
-
+  function QuickLoad() {
+    const { progress } = useProgress()
+    return <Html center> {progress} % loaded</Html>
+  }
   
   
   return (
@@ -492,7 +495,7 @@ function EyeAnimation({ preset }) {
         <EyeAnimation preset={preset} />
    <OrbitControls minDistance={10} maxDistance={200} makeDefault/>
       
-    <Suspense fallback={null}>    
+    <Suspense fallback={<QuickLoad/>}>    
           {(
             
             <>
@@ -529,7 +532,7 @@ function EyeAnimation({ preset }) {
         </EffectComposer>
       
       </Canvas>
-      <Loader/>
+      {/* <Loader/> */}
 
 
     </div>
